783bdc0d5ff36c63094c3a08fff0e446aa04b962
Moebytes <pie4chan@gmail.com>
2026-08-29 00:06:46 -0400

    update to typescript 7

--- functions/CompareFunctions.ts
+++ functions/CompareFunctions.ts
@@ -35,22 +35,22 @@ export default class CompareFunctions {
             if (imgBuffer.byteLength && functions.file.isImage(imgLink)) {
                 let isAnimated = functions.file.isAnimatedWebp(imgBuffer)
                 if (!isAnimated) isAnimated = functions.file.isAnimatedPng(imgBuffer)
-                if (!isAnimated) imgBuffer = decryption.decrypt(imgBuffer, privateKey, serverPublicKey, session)
+                if (!isAnimated) imgBuffer = decryption.decrypt(imgBuffer, privateKey, serverPublicKey, session).buffer
             }
             if (currentBuffer.byteLength && functions.file.isImage(currentLink)) {
                 let isAnimated = functions.file.isAnimatedWebp(currentBuffer)
                 if (!isAnimated) isAnimated = functions.file.isAnimatedPng(currentBuffer)
-                if (!isAnimated) currentBuffer = decryption.decrypt(currentBuffer, privateKey, serverPublicKey, session)
+                if (!isAnimated) currentBuffer = decryption.decrypt(currentBuffer, privateKey, serverPublicKey, session).buffer
             }
             if (upscaledImgBuffer.byteLength && functions.file.isImage(upscaledImgLink)) {
                 let isAnimated = functions.file.isAnimatedWebp(upscaledImgBuffer)
                 if (!isAnimated) isAnimated = functions.file.isAnimatedPng(upscaledImgBuffer)
-                if (!isAnimated) upscaledImgBuffer = decryption.decrypt(upscaledImgBuffer, privateKey, serverPublicKey, session)
+                if (!isAnimated) upscaledImgBuffer = decryption.decrypt(upscaledImgBuffer, privateKey, serverPublicKey, session).buffer
             }
             if (upscaledCurrentBuffer.byteLength && functions.file.isImage(currentUpscaledLink)) {
                 let isAnimated = functions.file.isAnimatedWebp(upscaledCurrentBuffer)
                 if (!isAnimated) isAnimated = functions.file.isAnimatedPng(upscaledCurrentBuffer)
-                if (!isAnimated) upscaledCurrentBuffer = decryption.decrypt(upscaledCurrentBuffer, privateKey, serverPublicKey, session)
+                if (!isAnimated) upscaledCurrentBuffer = decryption.decrypt(upscaledCurrentBuffer, privateKey, serverPublicKey, session).buffer
             }
 
             if (imgBuffer.byteLength) {
